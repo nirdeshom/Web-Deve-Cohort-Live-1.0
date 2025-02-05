2e6f767095405fe03646385f820e2f6991f99b50
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-02-06 03:32:49 +0530

    create factory

---
+++ JavaScript/hello.js
@@ -0,0 +1,16 @@
+function printChai(){
+    console.log("Hello Chai");
+}
+
+function bringBrush(numberOfBrush){
+    console.log(`Hanji le aaye brush ${numberOfBrush}`);
+}
+
+function addTwo(num1, num2){
+    return num1 + num2;
+}
+
+bringBrush(40);
+printChai();
+addTwo(5, 7);
+console.log(addTwo(2, 2));
