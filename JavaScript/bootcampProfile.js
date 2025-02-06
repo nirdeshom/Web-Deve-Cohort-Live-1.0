17d1fd1f04117b56c7f616dd9b9a3b2066b710c1
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-02-06 22:49:31 +0530

    Data Types

---
+++ JavaScript/bootcampProfile.js
@@ -0,0 +1,26 @@
+// string - "", '', ``
+let name = "Nirdesh";
+
+// Number - 25, 35.7
+let age = 22;
+
+// Boolean - true, false
+let isPaid = true;
+
+let favoriteClass = null;
+
+let homeTown;
+
+let skills = ["JavaScript", "PHP", "Python", "Ruby", "C Programming"]
+
+let studentProfile = {
+    name: "Nirdesh",
+    age: 22,
+    isPaid: true,
+    skills: ["HTML", "CSS", "JavaScript"],
+    favoriteClass: null
+}
+
+console.log(studentProfile);
+console.log(typeof studentProfile);
+console.log(typeof (favoriteClass));
