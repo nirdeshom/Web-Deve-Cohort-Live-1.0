0ac9b9e61d01c8f494021a63cffc67a762ace9e2
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-02-06 22:50:10 +0530

    condition

---
+++ JavaScript/outfit.js
@@ -0,0 +1,20 @@
+// server call rainy, cloudy, sunny
+
+let weather = "cloudy";
+
+if (weather === "rainy"){
+    console.log("it's raining, umbrela le jaana bhai");
+} else if (weather === "cloudy"){
+    console.log("jacket le jana bhai");
+} else {
+    console.log("sunny hai aaj to");
+}
+
+
+// if (){
+//     // code
+// } else if (){
+//     // code
+// } else {
+//     // code
+// }
