ea2a2e98d3b5366fd736b15f6423b8d14f78e5b7
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-02-08 22:42:39 +0530

    basic array

---
+++ JavaScript/array.js
@@ -0,0 +1,22 @@
+let fruits = ["apple", "cherry", "banana"];
+let intFruits = new Array("kiwi", "avacado", "dragon fruits");
+
+// fruits[0] = "blueberry";
+
+// fruits.push("new itam");
+// fruits.pop();
+
+// fruits.unshift("mango");
+// fruits.shift();
+
+// console.log(typeof fruits);
+
+// console.log(typeof intFruits);
+
+// console.log(fruits);
+
+// console.log(fruits.length);
+
+// console.log(fruits[0]);
+
+
