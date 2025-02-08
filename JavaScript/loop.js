8899e0e3dac9b4a00eb1095fdf667c5172855904
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-02-08 22:43:05 +0530

    basic loop

---
+++ JavaScript/loop.js
@@ -0,0 +1,16 @@
+let teas = ["masala", "ginger", "oolong"];
+
+// teas[h]
+
+// teas.length  // 6
+
+// let h = 6  // h <= 6 || h < 6
+
+// loop - iteration;
+// variable leke aao 
+// limit btao 
+// incre/decre 
+
+for(let h = 0; h < teas.length; h++){
+    console.log(`Fruits at index ${h}: ${teas[h]}`);
+}
