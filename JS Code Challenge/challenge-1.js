c077bc085be190c69e7f433eb9eb81c7a1cbc012
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-02-09 23:21:34 +0530

    conditional challenge 1: check even and odd num

---
+++ JS Code Challenge/challenge-1.js
@@ -0,0 +1,15 @@
+// Check Even and Odd Number 
+
+function checkEvenOdd(num){
+    if(num % 2 === 0){
+        return "Even"
+    }
+    else{
+        return "Odd"
+    }
+}
+
+let result = checkEvenOdd(6);
+console.log(result);
+
+// console.log(checkEvenOdd(5));
